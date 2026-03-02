f3891e0edcff9972a198c42d9657834a23ee4538
Tanmay Kadam <tanmaykadam260802@gmail.com>
2026-03-02 00:28:17 -0500

    Tic Tac Toe basic code

---
+++ app.js
@@ -0,0 +1,89 @@
+let boxes = document.querySelectorAll('.box');
+let resetButton = document.querySelector('#reset-button');
+let newGameButton = document.querySelector('#play-again-button');
+let messageContainer = document.querySelector('.msg-container');
+let message = document.querySelector('#message');
+
+let currentPlayer = 'X';
+
+let gameActive = true;
+
+const winningConditions = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+function handleBoxClick(index) { 
+    const box = boxes[index]
+    if (currentPlayer === 'X') {
+        box.innerText = 'X';
+        currentPlayer = 'O';
+    } else {
+        box.innerText = 'O';
+        currentPlayer = 'X';
+    }
+    box.disabled = true;
+
+    checkWin();
+}
+
+boxes.forEach( (box,index) => {
+    box.addEventListener('click', () => {
+        handleBoxClick(index)
+    }); 
+});
+
+const checkWin = () => {
+    for (let patterns of winningConditions) {   
+        console.log(patterns);
+        const [a, b, c] = patterns;
+        let boxA = boxes[a].innerText;
+        let boxB = boxes[b].innerText;
+        let boxC = boxes[c].innerText;
+
+        // console.log(boxes[a].innerText, boxes[b].innerText, boxes[c].innerText);
+
+        if (boxA != '' || boxB != '' || boxC != '') {
+            if (boxA && boxA === boxB && boxA === boxC) {
+                // alert(`Player ${boxA} wins!`);
+                gameActive = false;
+                showWinner(boxA);
+                // return;
+            }
+        }   
+    }
+}
+
+const showWinner = (winner) => {
+    console.log(`Player ${winner} wins!`);
+    message.innerText = `Player ${winner} wins!`;
+    console.log(message.innerText);
+    messageContainer.classList.remove('hide');
+}
+
+newGameButton.addEventListener('click', () => {
+    boxes.forEach(box => {
+        box.innerText = '';
+        box.disabled = false;
+    });
+    messageContainer.classList.add('hide');
+    currentPlayer = 'X';
+    gameActive = true;
+});
+
+resetButton.addEventListener('click', () => {
+    boxes.forEach(box => {
+        box.innerText = '';
+        box.disabled = false;
+    });
+    currentPlayer = 'X';
+    gameActive = true;
+}); 
+
+
